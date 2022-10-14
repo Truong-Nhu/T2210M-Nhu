9735c548d4205c4900acc832c3b48f4f1445ba22
Truong-Nhu <truongvannhu196@gmail.com>
2022-10-14 08:30:29 +0700

    Nop bai tap buoi 1

---
+++ BaiTap/ThuongSo.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	int b;
+	
+	printf("nhap so a:");
+	scanf("%d",&a);
+	printf("nhap so b:");
+	scanf("%d",&b);
+	int c = a / b;
+
+	printf("Thuong so cua a chia b là: %d", c);
+
+}
+
